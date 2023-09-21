3b949c8083cef427f74c61ea53fb1adfffeca10d
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2023-09-21 20:00:57 +0200

    New functionality added to heatmap.py
    Instead of reading data from a file stored locally within the project, it reads data from the database and returns it to the frontend, and with that data read from the database we work with

--- backend/src/components/heatmap.py
+++ backend/src/components/heatmap.py
@@ -1,25 +1,30 @@
 import os
 import json
+from pymongo import MongoClient
 import pandas as pd
 from flask import Flask, jsonify
 
 
 
 def load_data():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    json_file_path = os.path.join(dir_path, 'Data', 'data_01.json')
-    with open(json_file_path) as f:
-        data = json.load(f)
+    
+    MONGO_URI = 'mongodb://127.0.0.1'
+    client = MongoClient(MONGO_URI)
+    db = client['TFG-DATASETS']
+    twitter_collection = db["data_01.json"]
+
+    data = list(twitter_collection.find({}))
 
     map_data = pd.DataFrame(data)
     map_data['Compound'] = map_data['Compound'].astype(float)
 
-    # Drop rows with NaN values in latitude or longitude
+    # Eliminamos los valores NaN de las filas en latitude y longitude
     map_data = map_data.dropna(subset=['latitude', 'longitude'])
 
     #print(map_data)
-
     return map_data
+
+
 
 def draw_map():
     map_data = load_data()
