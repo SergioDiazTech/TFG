b36497a8137874a1a9e604a4b9ff45c6f222c0ed
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2023-09-26 22:29:27 +0200

    Update Heatmap.js

--- frontend/src/components/Heatmap.js
+++ frontend/src/components/Heatmap.js
@@ -1,83 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, LayerGroup, CircleMarker } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet.heat/dist/leaflet-heat.js';
 
 const API = process.env.REACT_APP_API;
+let mapInstance = null; // Variable para almacenar la instancia del mapa
 
 function Heatmap() {
   const [heatmapData, setHeatmapData] = useState([]);
 
   useEffect(() => {
+    // Limpiar la instancia del mapa anterior si existe
+    if (mapInstance) {
+      mapInstance.remove();
+      mapInstance = null; // Restablecer la variable de la instancia del mapa
+    }
+
     fetch(`${API}/heatmap`)
       .then(response => response.json())
       .then(data => {
-        if (data.longitude && data.latitude && data.compound) {
-          const heatmapPoints = data.longitude.map((lng, index) => ({
-            lng,
-            lat: data.latitude[index],
-            value: data.compound[index]
+        if (data.latitude && data.longitude && data.compound) {
+          const heatMapData = data.latitude.map((lat, index) => ({
+            lat,
+            lng: data.longitude[index],
+            value: data.compound[index],
           }));
-          setHeatmapData(heatmapPoints);
+
+          // Crear una nueva instancia del mapa
+          mapInstance = L.map('heatmap-map').setView([0, 0], 5);
+
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors',
+          }).addTo(mapInstance);
+
+          const gradient = {
+            0.5: 'blue',
+            0.6: 'cyan',
+            0.7: 'lime',
+            0.8: 'yellow',
+            0.9: 'orange',
+            1.0: 'red',
+          }; // Ajusta los colores y los valores aquí
+
+          L.heatLayer(heatMapData, {
+            radius: 15, // Ajusta el radio según tus necesidades
+            gradient,   // Aplica el gradiente de colores
+            blur: 10,   // Ajusta el desenfoque
+            maxZoom: 25, // Ajusta el zoom máximo
+            minOpacity: 0.4, // Ajusta la opacidad mínima
+          }).addTo(mapInstance);
+
+          mapInstance.fitBounds(L.latLngBounds(heatMapData.map(point => [point.lat, point.lng])));
+        } else {
+          console.error('Data is missing required properties (latitude, longitude, compound):', data);
         }
-        console.log(data.compound);
       })
       .catch(error => {
         console.error('Error fetching heatmap data:', error);
       });
   }, []);
 
-  const calculateCenter = () => {
-    if (heatmapData.length === 0) {
-      return [0, 0]; // Valor predeterminado en caso de que no haya datos
-    }
-
-    const sumLat = heatmapData.reduce((sum, point) => sum + point.lat, 0);
-    const sumLng = heatmapData.reduce((sum, point) => sum + point.lng, 0);
-    const avgLat = sumLat / heatmapData.length;
-    const avgLng = sumLng / heatmapData.length;
-
-    return [avgLat, avgLng];
-  };
-
-  return (
-    <div className="heatmap-wrapper">
-      <div className="heatmap-title">Mapa de calor en base a los tweets cargados</div>
-      <div className="heatmap-container">
-        <MapContainer center={calculateCenter()} zoom={5} className="heatmap-map">
-          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          <LayerGroup>
-            {heatmapData.map((point, index) => {
-              const { lat, lng, value } = point;
-              if (typeof lat !== 'number' || typeof lng !== 'number' || typeof value !== 'number') {
-                console.error(`Invalid data at index ${index}:`, point);
-                return null;
-              }
-
-              return (
-                <CircleMarker
-                  key={index}
-                  center={[lat, lng]}
-                  radius={6}
-                  fillOpacity={0.5}
-                  fillColor={getFillColor(value)}
-                  className="heatmap-marker"
-                />
-              );
-            })}
-          </LayerGroup>
-        </MapContainer>
-      </div>
-    </div>
-  );
-}
-
-function getFillColor(value) {
-  const red = Math.floor((1 - value) * 255);
-  const green = Math.floor((value + 1) * 255) / 2;
-  const blue = 0;
-  const opacity = 0.7;
-
-  return `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+  return <div id="heatmap-map" className="heatmap-map"></div>;
 }
 
 export default Heatmap;
