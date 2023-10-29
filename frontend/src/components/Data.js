fe419ff1a8309ca9f66a36e4da11298464abc191
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2023-10-29 20:12:54 +0100

    Added functionality to sort by sentiment

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
 
 const API = process.env.REACT_APP_API;
 const TWEETS_PER_PAGE = 5;
@@ -9,46 +10,67 @@ function Data() {
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
 
+  const [sortOrder, setSortOrder] = useState("asc");
+
   const getTweets = async (page, per_page) => {
     try {
-      const response = await fetch(`${API}/tweets?page=${page}&per_page=${per_page}`);
+      const response = await fetch(
+        `${API}/tweets?page=${page}&per_page=${per_page}&sortOrder=${sortOrder}`
+      );
       const data = await response.json();
       setTweets(data);
       setIsLoading(false);
     } catch (error) {
-      console.log('API:', API);
-      console.error('Error al obtener los tweets:', error);
+      console.error("Error al obtener los tweets:", error);
     }
   };
 
   const [totalTweetsLoaded, setTotalTweetsLoaded] = useState(0);
 
   const loadMoreTweets = async () => {
-  setIsLoading(true);
-  const newPage = page + 1;
+    setIsLoading(true);
+    const newPage = page + 1;
 
-  try {
-    const response = await fetch(`${API}/tweets?page=${newPage}&per_page=${TWEETS_PER_PAGE}`);
-    const data = await response.json();
+    try {
+      const response = await fetch(
+        `${API}/tweets?page=${newPage}&per_page=${TWEETS_PER_PAGE}`
+      );
+      const data = await response.json();
 
-    // Calcula el número total de tweets que has cargado
-    const newTotalTweetsLoaded = totalTweetsLoaded + data.length;
+      // Calcula el número total de tweets que has cargado
+      const newTotalTweetsLoaded = totalTweetsLoaded + data.length;
 
-    setTweets((prevTweets) => [...prevTweets, ...data]);
-    setPage(newPage);
-    setTotalTweetsLoaded(newTotalTweetsLoaded);
-    setIsLoading(false);
-  } catch (error) {
-    console.error('Error al cargar más tweets:', error);
-  }
-};
-
-  
+      setTweets((prevTweets) => [...prevTweets, ...data]);
+      setPage(newPage);
+      setTotalTweetsLoaded(newTotalTweetsLoaded);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error al cargar más tweets:", error);
+    }
+  };
 
   useEffect(() => {
     getTweets(page, TWEETS_PER_PAGE);
     // eslint-disable-next-line
-  }, []);
+  }, [sortOrder]);
+
+  const handleSortClick = () => {
+    if (sortOrder === "asc") {
+      setSortOrder("desc");
+    } else {
+      setSortOrder("asc");
+    }
+  };
+
+  const sortedTweets = [...tweets];
+  
+  sortedTweets.sort((a, b) => {
+    if (sortOrder === "asc") {
+      return a.sentiment - b.sentiment;
+    } else {
+      return b.sentiment - a.sentiment;
+    }
+  });
 
   return (
     <div>
@@ -57,13 +79,18 @@ function Data() {
         <thead>
           <tr>
             <th>#</th>
-            <th>Sentiment</th>
+            <th onClick={handleSortClick}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                Sentiment
+                {sortOrder === "asc" ? <GoTriangleDown /> : <GoTriangleUp />}
+              </div>
+            </th>
             <th>Text</th>
             <th>Retweets</th>
           </tr>
         </thead>
         <tbody>
-          {tweets.map((tweet, index) => (
+          {sortedTweets.map((tweet, index) => (
             <tr key={tweet.id}>
               <td>{index + 1}</td>
               <td>
@@ -74,7 +101,7 @@ function Data() {
                 )}
                 <span>{tweet.sentiment}</span>
               </td>
-              <td style={{ textAlign: 'left' }}>{tweet.text}</td>
+              <td style={{ textAlign: "left" }}>{tweet.text}</td>
               <td>{tweet.public_metrics.retweet_count}</td>
             </tr>
           ))}
@@ -83,7 +110,7 @@ function Data() {
       {tweets.length >= page * TWEETS_PER_PAGE && (
         <div className="button-container">
           <button onClick={loadMoreTweets} disabled={isLoading}>
-            {isLoading ? 'Loading...' : 'Load more'}
+            {isLoading ? "Loading..." : "Load more"}
           </button>
         </div>
       )}
