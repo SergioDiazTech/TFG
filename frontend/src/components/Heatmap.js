dd25cc72b6c44b60b376949ebfb20ae7904a3358
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2023-11-03 13:48:05 +0100

    Updated the heatmap so that I only have to generate the data once
    Added the concept of context

--- frontend/src/components/Heatmap.js
+++ frontend/src/components/Heatmap.js
@@ -1,60 +1,84 @@
-import React, { useEffect, useRef } from 'react';
+import React, { createContext, useContext, useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet.heat/dist/leaflet-heat.js';
 
 const API = process.env.REACT_APP_API;
 
+// Creamos un contexto para almacenar los datos del mapa de calor
+const HeatMapDataContext = createContext();
+
+// Este es nuestro componente proveedor que envolverá nuestra aplicación
+export function HeatMapDataProvider({ children }) {
+  const [heatMapData, setHeatMapData] = useState(null);
+
+  // Aquí es donde buscaríamos los datos del mapa de calor
+  useEffect(() => {
+    fetch(`${API}/heatmap`)
+      .then(response => response.json())
+      .then(data => {
+        if (data.latitude && data.longitude && data.compound) {
+          const newHeatMapData = data.latitude.map((lat, index) => ({
+            lat,
+            lng: data.longitude[index],
+            value: data.compound[index],
+          }));
+
+          setHeatMapData(newHeatMapData); // Guardamos los datos en el estado
+        } else {
+          console.error('Data is missing required properties (latitude, longitude, compound):', data);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching heatmap data:', error);
+      });
+  }, []);
+
+  return (
+    <HeatMapDataContext.Provider value={heatMapData}>
+      {children}
+    </HeatMapDataContext.Provider>
+  );
+}
+
 function Heatmap() {
   const mapContainer = useRef(null); // Referencia al contenedor del mapa
+  const heatMapData = useContext(HeatMapDataContext); // Obtenemos los datos del mapa de calor del contexto
 
   useEffect(() => {
-    if (!mapContainer.current) {
-      // El contenedor del mapa no está disponible
+    if (!mapContainer.current || !heatMapData) {
+      // El contenedor del mapa no está disponible o los datos aún no se han cargado
       return;
     }
 
     let mapInstance = L.map(mapContainer.current).setView([0, 0], 5); // Inicializamos el mapa aquí
 
     mapInstance.whenReady(() => {
-      fetch(`${API}/heatmap`)
-        .then(response => response.json())
-        .then(data => {
-          if (data.latitude && data.longitude && data.compound) {
-            const heatMapData = data.latitude.map((lat, index) => ({
-              lat,
-              lng: data.longitude[index],
-              value: data.compound[index],
-            }));
-
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mapInstance);
-
-            const gradient = {
-              0.5: 'blue',
-              0.6: 'cyan',
-              0.7: 'lime',
-              0.8: 'yellow',
-              0.9: 'orange',
-              1.0: 'red',
-            };
-
-            L.heatLayer(heatMapData, {
-              radius: 15,
-              gradient,
-              blur: 10,
-              maxZoom: 25,
-              minOpacity: 0.4,
-            }).addTo(mapInstance);
-
-            mapInstance.fitBounds(L.latLngBounds(heatMapData.map(point => [point.lat, point.lng])));
-          } else {
-            console.error('Data is missing required properties (latitude, longitude, compound):', data);
-          }
-        })
-        .catch(error => {
-          console.error('Error fetching heatmap data:', error);
-        });
+      renderHeatmap(heatMapData);
     });
+
+    function renderHeatmap(data) {
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mapInstance);
+
+      const gradient = {
+        0.5: 'blue',
+        0.6: 'cyan',
+        0.7: 'lime',
+        0.8: 'yellow',
+        0.9: 'orange',
+        1.0: 'red',
+      };
+
+      L.heatLayer(data, {
+        radius: 15,
+        gradient,
+        blur: 10,
+        maxZoom: 25,
+        minOpacity: 0.4,
+      }).addTo(mapInstance);
+
+      mapInstance.fitBounds(L.latLngBounds(data.map(point => [point.lat, point.lng])));
+    }
 
     // Limpiamos en el retorno de la función useEffect
     return () => {
@@ -63,7 +87,7 @@ function Heatmap() {
         mapInstance.remove();
       }
     };
-  }, []); // Pasamos un array vacío para que useEffect se ejecute solo una vez
+  }, [heatMapData]); // Pasamos heatMapData como dependencia
 
   return (
     <div className="heatmap-wrapper">
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HeatMapDataProvider } from './components/Heatmap.js';
 
 /*import 'bootstrap/dist/css/bootstrap.css'*/
 import 'bootswatch/dist/lux/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Usamos HeatMapDataProvider para envolver el componente App, esto hará que los datos del mapa de calor estén disponibles para todos los componentes hijos de App
 root.render(
   <React.StrictMode>
-    <App />
+    <HeatMapDataProvider>
+      <App />
+    </HeatMapDataProvider>
   </React.StrictMode>
 );
 
