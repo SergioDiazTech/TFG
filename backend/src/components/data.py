dd0f43617d0870dd8ecff133581a2df20f6ca0cd
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2023-11-09 15:28:54 +0100

    Update data.py

--- backend/src/components/data.py
+++ backend/src/components/data.py
@@ -20,14 +20,4 @@ def load_data(collection_name, page, per_page):
     skip = (page - 1) * per_page
     tweets = list(twitter_collection.find({"referenced_tweets": {"$exists": False}}).skip(skip).limit(per_page))
 
-
-    # Combina los campos de sentimiento en un solo campo llamado 'sentiment'
-    for tweet in tweets:
-        positive_sentiment = tweet.get('sentiment', {}).get('positive', 0)
-        negative_sentiment = tweet.get('sentiment', {}).get('negative', 0)
-
-        # Calcula el valor de 'sentiment' como la diferencia entre 'positive_sentiment' y 'negative_sentiment'
-        sentiment_value = round(positive_sentiment - negative_sentiment, 2)
-        tweet['sentiment'] = sentiment_value
-
     return tweets
