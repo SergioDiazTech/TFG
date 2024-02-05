28096208e0ba3750167b7f87daa1bee8a1468744
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2024-02-05 18:22:16 +0100

    Update heatmap.py

--- backend/src/components/heatmap.py
+++ backend/src/components/heatmap.py
@@ -10,7 +10,6 @@ def draw_heatmap(min_lat=None, max_lat=None, min_lng=None, max_lng=None):
     twitter_collection = db[twitter_collection_name]
 
     match_stage = {"latitude": {"$exists": True, "$ne": None}, "longitude": {"$exists": True, "$ne": None}}
-    # Aplicar filtro si se proporcionan los parámetros de latitud y longitud
     if min_lat is not None and max_lat is not None and min_lng is not None and max_lng is not None:
         match_stage.update({
             "latitude": {"$gte": min_lat, "$lte": max_lat},
@@ -49,7 +48,7 @@ def draw_heatmap(min_lat=None, max_lat=None, min_lng=None, max_lng=None):
 
     min_sentiment = min(item['sentiment'] for item in data) if data else 0
     max_sentiment = max(item['sentiment'] for item in data) if data else 0
-    percentiles = np.percentile([item['sentiment'] for item in data], [0, 25, 50, 75, 100]) if data else [0,0, 0, 0, 0]
+    #percentiles = np.percentile([item['sentiment'] for item in data], [0, 25, 50, 75, 100]) if data else [0,0, 0, 0, 0]
 
 
     registry_document = db['Ingestion_Registry'].find_one({"CollectionName": twitter_collection_name})
@@ -60,7 +59,7 @@ def draw_heatmap(min_lat=None, max_lat=None, min_lng=None, max_lng=None):
         'coordinateGroups': coordinate_groups,
         'minSentiment': min_sentiment,
         'maxSentiment': max_sentiment,
-        'percentiles': percentiles.tolist(),
+        #'percentiles': percentiles.tolist(),
         'collectionName': collection_display_name,
         'globalSentiment': global_sentiment_mean
     }
