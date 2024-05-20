0532f06b33dc91d2df0ab563cda992fbb27b1728
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2024-05-20 21:01:36 +0200

    Creation of the service package for ingests

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from controllers.twitter_API import get_tweets
-from controllers.dataset import save_json_to_mongodb
+from services.twitter_API import get_tweets
+from services.dataset import save_json_to_mongodb
 from controllers.data import load_data, get_collection_names
 from controllers.heatmap import draw_heatmap
 from controllers.pointmap import draw_pointmap
