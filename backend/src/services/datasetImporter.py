d0b0e05e688d44406dbec30c998d423772c0f81d
Sergio Diaz de la Peña <sergiodp0604@gmail.com>
2024-06-05 00:21:24 +0200

    Added functionality to extract geographical references

--- backend/src/services/datasetImporter.py
+++ backend/src/services/datasetImporter.py
@@ -1,42 +1,115 @@
 from pymongo import MongoClient
+from datetime import datetime
+from geopy.geocoders import ArcGIS, Nominatim
+from geopy.exc import GeocoderTimedOut
+from collections import Counter
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+import time
 import json
 import os
-from datetime import datetime
-import time
+import spacy
+import re
+import nltk
 
-from geopy.geocoders import ArcGIS
+nltk.download('punkt')
+nltk.download('stopwords')
 
 DATA_FOLDER = '/Users/sergio/Desktop/TFG/backend/'
 MONGO_URI = 'mongodb://127.0.0.1'
 
-geolocator = ArcGIS()
+geolocator_arcgis = ArcGIS()
+geolocator_nominatim = Nominatim(user_agent="myGeocoder")
 
-def procesamiento_geoparsing(tweet):
-    address = tweet.get('location')
-    if address:
-        latitude, longitude = geolocate(address)
-        if latitude is not None and longitude is not None:
-            tweet['latitude'] = latitude
-            tweet['longitude'] = longitude
-            tweet['location'] = address  # Guardar la ubicación original solo si se obtienen coordenadas
-        else:
-            tweet['latitude'] = None
-            tweet['longitude'] = None
-            tweet.pop('location', None)  # Eliminar la ubicación si no se pueden obtener coordenadas
+# Cargar el modelo de lenguaje de spaCy
+nlp = spacy.load('es_core_news_sm')
+
+STOPWORDS = set(stopwords.words('spanish'))
+
+KNOWN_LOCATIONS = set([
+    "Bogotá", "Medellín", "Cali", "Barranquilla", "Cartagena", "Cúcuta",
+    "Bucaramanga", "Pereira", "Santa Marta", "Manizales", "Villavicencio",
+    "Armenia", "Ibagué", "Pasto", "Neiva", "Montería", "Popayán", "Valledupar",
+    "Tunja", "Riohacha", "Florencia", "Sincelejo", "Quibdó", "Mocoa", 
+    "San Andrés", "Yopal", "Inírida", "Puerto Carreño", "Leticia", 
+    "San José del Guaviare", "Casanare", "Arauca", "Girardot", "Turbo", 
+    "Rionegro", "Apartadó", "Soledad", "Malambo", "Magangué", "Sogamoso", 
+    "Duitama", "Zipaquirá", "Facatativá", "Chía", "Fusagasugá", "Cajicá", 
+    "Chocontá", "Mosquera", "Madrid", "Funza", "La Dorada", "Honda", 
+    "Arjona", "El Carmen de Bolívar", "El Banco", "Maicao", "Fonseca", 
+    "Ciénaga", "La Jagua de Ibirico", "Aguachica", "Ocaña", "Pamplona", 
+    "Tumaco", "Ipiales", "Tuluá", "Palmira", "Jamundí", "Yumbo", "Buga", 
+    "Buenaventura", "Cartago", "Floridablanca", "Girón", "Piedecuesta", 
+    "Barrancabermeja", "San Gil", "Socorro", "Lorica", "Sahagún", "Cereté",
+    "Planeta Rica", "Tierralta", "Chiquinquirá", "Garzón", "Pitalito", 
+    "La Plata", "Puerto Boyacá", "Guateque", "El Espinal", "Mariquita", 
+    "Líbano", "Saldaña", "El Guamo", "Chaparral", "Purificación", 
+    "San Vicente del Caguán", "La Montañita", "El Doncello", "Puerto Asís", 
+    "Orito", "Puerto Leguízamo", "Puerto Nariño", "Aipe", "Algeciras", 
+    "Barbosa", "Betania", "Betulia", "Caldas", "Carmen de Viboral", 
+    "Copacabana", "Dabeiba", "El Retiro", "Envigado", "Fredonia", "Frontino", 
+    "Giraldo", "Guarne", "La Ceja", "La Estrella", "La Pintada", "Marinilla", 
+    "Mutatá", "Rionegro", "Sabaneta", "Salgar", "Santa Bárbara", "Santa Fe de Antioquia", 
+    "Sonsón", "Titiribí", "Yalí", "Yarumal", "Yolombó", "Zaragoza"
+])
+
+
+def clean_text(text):
+    text = re.sub(r'@\w+', '', text)
+    text = re.sub(r'#\w+', '', text)
+    text = re.sub(r'http\S+|www.\S+', '', text)
+    return text
+
+def extract_location(text):
+    cleaned_text = clean_text(text)
+    doc = nlp(cleaned_text)
+    
+    locations = [ent.text for ent in doc.ents if ent.label_ == 'LOC']
+    
+    potential_locations = []
+    for token in doc:
+        if token.text.lower() not in STOPWORDS:
+            if token.pos_ in ['PROPN', 'ADJ'] and token.ent_type_ == 'LOC':
+                potential_locations.append(token.text)
+    
+    sentences = sent_tokenize(cleaned_text)
+    co_occurrence_locations = []
+    for sentence in sentences:
+        words = word_tokenize(sentence)
+        for word in words:
+            if word in KNOWN_LOCATIONS:
+                co_occurrence_locations.append(word)
+    
+    all_locations = locations + potential_locations + co_occurrence_locations
+    
+    location_counts = Counter(all_locations)
+    if location_counts:
+        most_common_location = location_counts.most_common(1)[0][0]
+        return most_common_location
     else:
-        tweet['latitude'] = None
-        tweet['longitude'] = None
-        tweet.pop('location', None)
-    return tweet
+        return ' '.join(locations)
 
+def geolocate(location, retries=3):
+    for attempt in range(retries):
+        try:
+            location_arcgis = geolocator_arcgis.geocode(location)
+            if location_arcgis:
+                return location_arcgis.latitude, location_arcgis.longitude
+        except GeocoderTimedOut:
+            print(f"ArcGIS geocode timeout, retrying {attempt + 1}/{retries}")
+        except Exception as e:
+            print(f'ArcGIS Error: {e}')
 
-def geolocate(location):
-    try:
-        location = geolocator.geocode(location)
-        if location:
-            return location.latitude, location.longitude
-    except Exception as e:
-        print(f'Error: {e}')
+        try:
+            location_nominatim = geolocator_nominatim.geocode(location)
+            if location_nominatim:
+                return location_nominatim.latitude, location_nominatim.longitude
+        except GeocoderTimedOut:
+            print(f"Nominatim geocode timeout, retrying {attempt + 1}/{retries}")
+        except Exception as e:
+            print(f'Nominatim Error: {e}')
+
     return None, None
 
 def calculate_sentiment(tweets):
@@ -86,6 +159,19 @@ def format_time(seconds):
     seconds = seconds % 60
     return f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
 
+def procesamiento_geoparsing(tweet):
+    address = tweet.get('location')
+    if address:
+        latitude, longitude = geolocate(address)
+        if latitude is not None and longitude is not None:
+            tweet['latitude'] = latitude
+            tweet['longitude'] = longitude
+            tweet['location'] = address
+        else:
+            tweet['latitude'] = None
+            tweet['longitude'] = None
+            tweet.pop('location', None)
+
 
 def save_json_to_mongodb(filename, custom_name):
     DATA_FILEPATH = os.path.join(DATA_FOLDER, filename)
@@ -94,8 +180,24 @@ def save_json_to_mongodb(filename, custom_name):
 
     client = MongoClient(MONGO_URI)
     db = client['DB_External_Data_Ingestion']
+
+    if filename == "geolocation.json":
+        dataset_collection = db['geolocation']
+        for document in data:
+            text = document.get('text', '')
+            location_name = extract_location(text)
+            latitude, longitude = geolocate(location_name)
+            
+            document['location_name'] = location_name
+            document['latitude'] = latitude
+            document['longitude'] = longitude
+
+            document['_id'] = document['_id']['$oid']
+            if dataset_collection.find_one({'_id': document['_id']}) is None:
+                dataset_collection.insert_one(document)
+                print(f"Document ID: {document['_id']} - Nuevo documento añadido con latitud: {document['latitude']} y longitud: {document['longitude']}")
     
-    if filename == "tweets_colombia21.json":
+    elif filename == "tweets_colombia21.json":
         dataset_collection = db['tweets_colombia']
         calculate_sentiment(data)
         calculate_hate_speech_score(data)
@@ -139,7 +241,6 @@ def save_json_to_mongodb(filename, custom_name):
                     dataset_collection.insert_one(user)
                     print(f"User ID: {user['_id']} - Nuevo usuario añadido con latitud: {user['latitude']} y longitud: {user['longitude']}")
 
-                # Actualizar coordenadas en los tweets correspondientes
                 tweets_to_update = tweets_collection.find({
                     'author_id': user['id'], 
                     'referenced_tweets': {'$exists': False}
@@ -174,9 +275,8 @@ def save_json_to_mongodb(filename, custom_name):
             print(f"Total de documentos procesados: {processed_count}")
 
             
-    os.remove(DATA_FILEPATH)  # Eliminar el archivo después de procesarlo
+    os.remove(DATA_FILEPATH)
 
-    # Registro de la ingesta
     registro_collection = db['Ingestion_Registry']
     documento_registro = {
         'Name': custom_name,
@@ -189,4 +289,3 @@ def save_json_to_mongodb(filename, custom_name):
     registro_collection.insert_one(documento_registro)
     print(documento_registro)
     print(f"Total documents: {dataset_collection.count_documents({})}")
-    #print(f"New documents added: {new_count}")
